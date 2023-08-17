1d3b13648493bfc4faa1e6d7d1ce4c63795c6b24
Prakash <105060234+s-prakashs@users.noreply.github.com>
2023-08-17 16:23:03 +0530

    services worked

--- assets/js/service.js
+++ assets/js/service.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
   
     subHeadings.forEach((heading) => {
       heading.addEventListener("click", () => {
+        console.log("Clicked")
+        
+        //const servicesList = document.querySelectorAll(".services-list .services");
+        //servicesList.classList.toggle("show");
         const services = heading.nextElementSibling;
         services.classList.toggle("show");
       });
