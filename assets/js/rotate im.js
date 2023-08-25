edadd071f613b2552841d3953810899f7f82ccb8
Prakash <105060234+s-prakashs@users.noreply.github.com>
2023-08-25 13:47:21 +0530

    rotate working

--- assets/js/rotate im.js
+++ assets/js/rotate im.js
@@ -15,3 +15,18 @@ options.forEach((option) => {
         title.innerHTML = this.textContent;
     });
 });
+
+const colorVariables = [
+    '--ultra-violet', '--ultra-violet-2', '--pomp-and-power', '--pomp-and-power-2',
+    '--chinese-violet', '--mulberry', '--mulberry-2', '--blush', '--blush-2', '--light-coral'
+];
+
+const divElements = document.querySelectorAll('.options > div');
+
+divElements.forEach((div, index) => {
+    const colorVariable = colorVariables[Math.floor(Math.random() * colorVariables.length)];
+    const delay = -1 * (index + 1);
+
+    div.style.animationDelay = `calc(var(--_speed) * ${delay}s)`;
+    div.style.color = `var(${colorVariable})`;
+});
