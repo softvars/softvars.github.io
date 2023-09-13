8e1e2c8ff4fac9f9c4736a5d5586223c8145811b
pavidinesh96 <108331088+pavidinesh96@users.noreply.github.com>
2023-09-13 11:17:57 +0000

    margin reduce

--- assets/js/service.js
+++ assets/js/service.js
@@ -1,21 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const subHeadings = document.querySelectorAll(".sub-heading");
-  
-    subHeadings.forEach((heading) => {
-      heading.addEventListener("mouseover", () => {
-        console.log("Clicked");
-        const services = heading.nextElementSibling;
-        var isOpen=services.classList.contains('show');
+var onpageload = function () {
+  const subHeadings = document.querySelectorAll(".sub-heading span");
+ 
+  subHeadings.forEach((heading) => {
+    ['mouseover', 'click','ontouchstart'].forEach( function(evt) {
+    heading.addEventListener(evt, () => {
+      console.log("Clicked");
+      const services = heading.parentElement.nextElementSibling;
+      var isOpen=services.classList.contains('show');
 
-        const servicesList = document.querySelectorAll(".services-list .services");
-        servicesList.forEach((services) =>{
-          services.classList.remove("show");
-        })
+      const servicesList = document.querySelectorAll(".services-list .services");
+      servicesList.forEach((services) =>{
+        services.classList.remove("show");
+      })
 
-        // servicesList.classList.remove("show");
-        services.classList.toggle("show", !isOpen) ;
-      });
+      // servicesList.classList.remove("show");
+      services.classList.toggle("show", !isOpen) ;
     });
   });
+
+  heading.addEventListener("mouseleave", () => {
+    console.log("Clicked");
+    const services = heading.parentElement.nextElementSibling;
+
+    const servicesList = document.querySelectorAll(".services-list .services");
+    servicesList.forEach((services) =>{
+      services.classList.remove("show");
+    })
+  });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", onpageload);
   
   
